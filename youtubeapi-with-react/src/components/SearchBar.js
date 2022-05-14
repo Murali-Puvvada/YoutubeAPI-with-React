05e1201d5c1b044683744e9e9997693f163d2b6c
Murali Puvvada <saipuvvada81299@gmail.com>
2022-05-14 20:18:53 +0200

    Search Component

---
+++ youtubeapi-with-react/src/components/SearchBar.js
@@ -0,0 +1,29 @@
+import React from "react"
+
+
+class SearchBar extends React.Component{
+  state={term:""}
+  handleSubmit=(event)=>{
+    event.preventDefault()
+  }
+
+  handleChange=(event)=>{
+    this.setState({term:event.target.value})
+  }
+
+  render() {
+    return (
+      <div className={"search-bar ui segment"}>
+        <form className={"ui form"} onSubmit={this.handleSubmit}>
+          <div className={"field"}>
+            <label>Video Search</label>
+            <input type={"text"}  value={this.state.term} onChange={this.handleChange} />
+
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default SearchBar;
